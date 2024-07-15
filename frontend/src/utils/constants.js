775792edb2b856a957e93a2c12296b803ed30ff6
arty gvozdenkov <gvozdenkov@gmail.com>
2024-07-15 17:15:13 +0300

    fix(frontend): change api url to localhost for local dev

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = 'https://api.gifty.nomorepartiesco.ru/api/v1';
+export const URL = 'http://localhost:4000/api/v1';
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
