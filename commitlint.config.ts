3a41d3304ab0d0c7d8b6670405cc7e18c11bfcc2
arty gvozdenkov <gvozdenkov@gmail.com>
2024-07-15 15:30:44 +0300

    feat: add pnpm workspace, copy front & back to /apps

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -4,7 +4,7 @@ const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   formatter: '@commitlint/format',
   helpUrl:
-    'https://github.com/conventional-changelog/commitlint/#what-is-commitlint or !!! use "yarn cz" to step by step commit creation',
+    'https://github.com/conventional-changelog/commitlint/#what-is-commitlint or !!! use "pnpm run cz" to step by step commit creation',
 };
 
 export default Configuration;
